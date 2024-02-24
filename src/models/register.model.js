d086f54a5f005d5e7a9722e21357dd54de47a8bb
RicardoVerp <cmicveracruz17@hotmail.com>
2024-02-23 22:58:53 -0600

    Primer Versión

--- src/models/register.model.js
+++ src/models/register.model.js
@@ -3,27 +3,26 @@ import mongoose  from "mongoose";
 const registerCollection = 'registers'
 
 const registerSchema = new mongoose.Schema({
- name_complete: String,
- email:String,
- tel_oficina:Number,
- num_cel:Number,
- nom_emp:String,
- afil_cmic:String,
- del_cmic:String,
- int_afil:String,
- handicap:String,
- talla:String,
- carrito:String,
- factura:String,
-razon_social:String,
-rfc_fact:String,
-dom_fact:String,
-cod_Fiscal:Number,
-cfdi_fact:String,
-regFiscal_fact:String,
-email_fact:String,
-
-})
+  name_complete: String,
+  email: { type: String, unique: false },
+  tel_oficina: Number,
+  num_cel: Number,
+  nom_emp: String,
+  afil_cmic: String,
+  del_cmic: String,
+  int_afil: String,
+  handicap: String,
+  talla: String,
+  carrito: String,
+  factura: String,
+  razon_social: String,
+  rfc_fact: String,
+  dom_fact: String,
+  cod_Fiscal: Number,
+  cfdi_fact: String,
+  regFiscal_fact: String,
+  email_fact: String,
+});
 
 mongoose.set('strictQuery',false)
 
